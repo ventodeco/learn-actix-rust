f57d01d03532ce90b6949df68251914d2e03247a
Yosep Novento Nugroho <yosep.nugroho@gopay.co.id>
2024-03-31 19:38:32 +0700

    [extractor app data] using app state

--- section-1/extractor-app-data/src/main.rs
+++ section-1/extractor-app-data/src/main.rs
@@ -1,44 +1,40 @@
-use std::sync::Mutex;
-
 use actix_web::{App, HttpServer};
 
 
+#[derive(Clone)]
 struct Messenger {
     message: String
 }
 
-struct MutableState {
-    messenger: Mutex<Messenger>
+#[derive(Clone)]
+struct AppState {
+    messenger: Messenger
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    let app_data = actix_web::web::Data::new(MutableState {
-        messenger: Mutex::new(Messenger {
-            message: "hello".to_string()
-        })
-    });
+    let app_state = AppState {
+        messenger: Messenger { message: "hello".to_string() }
+    };
 
     HttpServer::new(move || {
         App::new()
-            .app_data(app_data.clone())
+            .app_data(actix_web::web::Data::new(app_state.clone()))
                 .route("/", actix_web::web::post().to(update))
                 .route("/", actix_web::web::get().to(get))
     })
     .bind(("127.0.0.1", 8000))?
     .run()
     .await
-
 }
 
-async fn update(app_data: actix_web::web::Data<MutableState>) -> String {
+async fn update(app_data: actix_web::web::Data<AppState>) -> String {
 
-    let mut messenger = app_data.messenger.lock().unwrap();
-    messenger.message = format!("{} world", messenger.message);
-    "".to_string()
+    let messenger = app_data.messenger.clone();
+    format!("{} world", messenger.message)
 }
 
-async fn get(app_data: actix_web::web::Data<MutableState>) -> String {
-    app_data.messenger.lock().unwrap().message.clone()
+async fn get(app_data: actix_web::web::Data<AppState>) -> String {
+    app_data.messenger.message.clone()
 }
